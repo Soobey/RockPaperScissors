967b781b8051285277c2e341496faedf1cbd8a78
Szabolcs Ambrus <a.sabi@gmx.de>
2022-04-02 12:15:50 +0200

    create main outcome function

--- javascript.js
+++ javascript.js
@@ -14,8 +14,44 @@ function computerPlay() {
         return 'Scissors';
     }
     else {
-        return x = "Something went wrong";
+        return error = 'Something went wrong';
     }
 }
 
-console.log(computerPlay())
+function Play(playerSelection, computerSelection) {
+    if (playerSelection === 'Rock') {
+        if (computerSelection === 'Rock') {
+            return result = 'Draw';
+        }
+        else if (computerSelection === 'Paper') {
+            return result = 'You Lose! Paper beats Rock';
+        }
+        else {
+            return result = 'You Win! Rock beats Scissors';
+        }
+    }
+    else if (playerSelection === 'Paper') {
+        if (computerSelection === 'Rock') {
+            return result = 'You Win! Paper beats Rock';
+        }
+        else if (computerSelection === 'Paper') {
+            return result = 'Draw';
+        }
+        else {
+            return result = 'You Lose! Scissors beat Paper';
+        }
+    }
+    else {
+        if (computerSelection === 'Rock') {
+            return result = 'You Lose! Rock beats Scissors';
+        }
+        else if (computerSelection === 'Paper') {
+            return result = 'You Win! Scissors beats Paper';
+        }
+        else {
+            return result = 'Draw';
+        }        
+    }
+}
+
+console.log(Play('Paper', 'Rock'))
