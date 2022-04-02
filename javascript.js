10333a1246815bac530f47acbf5b7fe1ad88563d
Szabolcs Ambrus <a.sabi@gmx.de>
2022-04-02 11:59:03 +0200

    create function with return Rock, Paper or Scissor

--- javascript.js
+++ javascript.js
@@ -1,3 +1,21 @@
 function randNum(min = 0, max = 1) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
+
+function computerPlay() {
+    let choice = randNum(1, 3)
+    if (choice === 1) {
+        return 'Rock';
+    }
+    else if (choice === 2) {
+        return 'Paper';
+    }
+    else if (choice === 3) {
+        return 'Scissors';
+    }
+    else {
+        return x = "Something went wrong";
+    }
+}
+
+console.log(computerPlay())
